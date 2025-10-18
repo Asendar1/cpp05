5ba2643bbbc98e8bdd85bd82bb97f65c893b59b5
Hamzah <hamzahtaimour@gmail.com>
2025-10-18 18:07:19 +0300

    ex01 is done

---
+++ ex01/Bureaucrat.hpp
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <exception>
+#include "Form.hpp"
+
+class Form;
+
+class Bureaucrat
+{
+	private:
+		std::string const 	_name;
+		int 				_grade;
+	public:
+		Bureaucrat();
+		Bureaucrat(std::string name, int grade);
+		~Bureaucrat();
+		Bureaucrat(const Bureaucrat &o);
+		Bureaucrat &operator=(const Bureaucrat &o);
+
+		int	getGrade() const;
+		std::string getName() const;
+
+		void incrementGrade(); // -1
+		void decrementGrade(); // +1
+
+		void signForm(Form &form) const;
+
+		class GradeTooHighException : public std::exception
+		{
+			public:
+				virtual const char* what() const throw()
+				{
+					return ("Grade is too high");
+				}
+		};
+		class GradeTooLowException : public std::exception
+		{
+			public:
+				virtual const char* what() const throw()
+				{
+					return ("Grade is too low");
+				}
+		};
+};
+
+std::ostream &operator<<(std::ostream &out, const Bureaucrat &b);
---
+++ ex01/main.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include "Bureaucrat.hpp"
+#include "Form.hpp"
+
+int main()
+{
+	Bureaucrat a;
+	Bureaucrat b("bob", 1);
+	Bureaucrat c(b);
+	Bureaucrat d;
+	d = b;
+
+	std::cout << b << std::endl;
+
+	try
+	{
+		b.incrementGrade();
+	}
+	catch(const std::exception &e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+
+	Bureaucrat e("alice", 150);
+	std::cout << e << std::endl;
+
+	try
+	{
+		e.decrementGrade();
+	}
+	catch(const std::exception &e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+
+	Form f("FormA", 50, 100);
+	std::cout << f << std::endl;
+	try
+	{
+		b.signForm(f);
+	}
+	catch(const std::exception &e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+	std::cout << f << std::endl;
+	try
+	{
+		f.beSigned(e);
+	}
+	catch(const std::exception &ex)
+	{
+		std::cerr << ex.what() << std::endl;
+	}
+	try
+	{
+		e.signForm(f);
+	}
+	catch(const std::exception &ex)
+	{
+		std::cerr << ex.what() << std::endl;
+	}
+	return 0;
+}
